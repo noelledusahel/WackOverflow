fd81ab7dfa12fdbc7c401b625bbdc096e7590022
JD Hiudt & Kristen Moore & Noelle Barber & Sarah Dudek <pair+jd-hiudt.kmoreo.noelledusahel.sarahdudek@devbootcamp.com>
2017-02-23 17:31:47 -0600

    add a function to add question linked to a user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,24 @@
+
 get '/questions' do
   @questions = Question.all
   erb :'questions/index'
 end
 
+get '/questions/new' do
+  erb :'questions/new'
+end
+
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   erb :'questions/show'
+end
+
+post '/questions' do
+  @question = Question.new(title: params[:title], content: params[:content], author_id: session[:user_id] )
+  if @question.save
+    redirect '/questions'
+  else
+    @errors = @question.errors.full_message
+    erb :'questions/new'
+  end
 end
