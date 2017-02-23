e16a19952cdf5b9557df07e2307f2f8b2df9acd6
JD Hiudt & Kristen Moore & Noelle Barber & Sarah Dudek <pair+jd-hiudt.kmoreo.noelledusahel.sarahdudek@devbootcamp.com>
2017-02-23 12:24:07 -0600

    begin to write models, unfinished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many: :questions, foreign_key: :author_id
+  has_many: :answers, foreign_key: :responder_id
+  has_many: :comments, as: :commentable
+  has_many: :votes, as: :votable
+
 end
