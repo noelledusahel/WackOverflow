d348e228619f4496cd370a699b98b5fcebc903e4
Noelle Barber & Sarah Dudek <pair+noelledusahel.sarahdudek@devbootcamp.com>
2017-02-23 15:53:38 -0600

    Create user registration and sessions routes. Add password methods to user model

---
+++ app/controllers/user_controllers.rb
@@ -0,0 +1,17 @@
+enable :sessions
+
+get '/users/new' do
+ erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    set_user(@user)
+    redirect "/"
+  else
+    status 422
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,34 @@ class User < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
 
+  validates :username, presence: true
+  validate :validate_password
+
+  def password
+    @password ||= Password.new(self.hashed_password)
+  end
+
+  def password=(input_password)
+    @raw_password = input_password
+    @password = Password.create(input_password)
+    self.hashed_password = @password
+  end
+
+  def validate_password
+    if @raw_password.nil?
+      errors.add(:password, "is required")
+    elsif @raw_password.length < 6
+      errors.add(:password, "must be greater than 6 characters")
+    end
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(username: username)
+
+    if user && user.password == password
+      return user
+    else
+      return nil
+    end
+  end
 end
