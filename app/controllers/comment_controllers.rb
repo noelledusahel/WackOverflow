def9901a4b61033f2b97e4f13293f28ad9a57ac8
Sarah Dudek <email": null>
2017-02-24 17:44:21 -0600

    Begin ajax

--- app/controllers/comment_controllers.rb
+++ app/controllers/comment_controllers.rb
@@ -1,7 +1,22 @@
 post '/questions/:id/comments' do
+  binding.pry
   id = params[:id].to_i
   question = Question.find(id)
   question.comments.create(body: params[:body], commenter_id: current_user.id)
+
+  if response.xhr?
+    content_type :json
+    {body: question.comments.body}.to_json
+    erb :'/comment/one_comment'
+  else
+    redirect "/questions/#{question.id}"
+  end
+end
+
+delete '/questions/:question_id/comments/:id' do
+  question = Question.find(params[:question_id])
+  target_comment = question.comments.find(params[:id])
+  target_comment.destroy
 
   redirect "/questions/#{question.id}"
 end
--- public/js/application.js
+++ public/js/application.js
@@ -34,5 +34,26 @@ $(document).ready(function() {
       $("#existing_user").hide();
     })
   });
+
+  $('#question-comment-form').on("click", '#submit-button', function(e){
+    e.preventDefault();
+
+    var textBar = $(this).closest('#question-comment-form').find('#input-text')
+    var data = textBar.val()
+      // debugger
+
+    var formDiv = $(this).closest('#question-comment-form').find('form')
+    var url = formDiv.attr('action')
+    var type = formDiv.attr('method')
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+    })
+    .done(function(response) {
+      console.log(response);
+    })
+  })
 });
 
