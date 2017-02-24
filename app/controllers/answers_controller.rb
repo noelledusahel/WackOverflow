b0b0fc7370362fdbabda8385fed2c223d0dbe634
JD Hiudt & Kristen Moore & Noelle Barber & Sarah Dudek <pair+jd-hiudt.kmoreo.noelledusahel.sarahdudek@devbootcamp.com>
2017-02-24 16:37:54 -0600

    create vote button and added routes to create a new vote

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,8 +2,6 @@ enable :sessions
 
 post '/questions/:id/answers' do
   @answer = Answer.new(content: params[:content], question_id: params[:id], responder_id: session[:user_id])
-  # @answers = Answer.all
-  # # .find_by(question_id: params[:id])
 
   if @answer.save
     redirect "/questions/#{params[:id]}"
---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,13 @@
+enable :sessions
+
+post '/questions/:id/votes' do
+  @question = Question.find_by(id: params[:id])
+
+  if params[:value] == "WORD!"
+    @vote = question.votes.create(value: true, voter_id: current_user.id)
+  else
+    @vote = question.votes.create(value: false, voter_id: current_user.id)
+  end
+
+  redirect "/questions/#{@question.id}"
+end
